cb7e9299cac78bd640a17f012a650843c22e495d
root <root@monbelle.node.dockerapp.io>
2016-09-04 11:08:21 -0400

    install

---
+++ dev/tools/grunt/configs/themes.js
@@ -0,0 +1,224 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+'use strict';
+
+/**
+ * Define Themes
+ *
+ * area: area, one of (frontend|adminhtml|doc),
+ * name: theme name in format Vendor/theme-name,
+ * locale: locale,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+module.exports = {
+    blank: {
+        area: 'frontend',
+        name: 'Magento/blank',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+            'css/email',
+            'css/email-inline'
+        ],
+        dsl: 'less'
+    },
+    luma: {
+        area: 'frontend',
+        name: 'Magento/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_default: {
+        area: 'frontend',
+        name: 'Emthemes/everything_default',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_glass: {
+        area: 'frontend',
+        name: 'Emthemes/everything_glass',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_shoes: {
+        area: 'frontend',
+        name: 'Emthemes/everything_shoes',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_jewelry: {
+        area: 'frontend',
+        name: 'Emthemes/everything_jewelry',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_furniture: {
+        area: 'frontend',
+        name: 'Emthemes/everything_furniture',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_fashionnova: {
+        area: 'frontend',
+        name: 'Emthemes/everything_fashionnova',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_bicycle: {
+        area: 'frontend',
+        name: 'Emthemes/everything_bicycle',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_clothing: {
+        area: 'frontend',
+        name: 'Emthemes/everything_clothing',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_baby: {
+        area: 'frontend',
+        name: 'Emthemes/everything_baby',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_swatch: {
+        area: 'frontend',
+        name: 'Emthemes/everything_swatch',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_lingerie: {
+        area: 'frontend',
+        name: 'Emthemes/everything_lingerie',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_beauty: {
+        area: 'frontend',
+        name: 'Emthemes/everything_beauty',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_health: {
+        area: 'frontend',
+        name: 'Emthemes/everything_health',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_supermarket_store: {
+        area: 'frontend',
+        name: 'Emthemes/everything_supermarket_store',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    everything_garden: {
+        area: 'frontend',
+        name: 'Emthemes/everything_garden',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_computer: {
+        area: 'frontend',
+        name: 'Emthemes/everything_computer',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+	everything_sport: {
+        area: 'frontend',
+        name: 'Emthemes/everything_sport',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    backend: {
+        area: 'adminhtml',
+        name: 'Magento/backend',
+        locale: 'en_US',
+        files: [
+            'css/styles-old',
+            'css/styles'
+        ],
+        dsl: 'less'
+    }
+};
